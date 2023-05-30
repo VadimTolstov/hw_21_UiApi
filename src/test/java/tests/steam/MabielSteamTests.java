a7554143f204c492aba63d5208bf4e63cd4e1dc2
VadimTolstov <tolstov50@mail.ru>
2023-05-30 18:42:57 +0300

    v 0.94

---
+++ src/test/java/tests/steam/MabielSteamTests.java
@@ -0,0 +1,137 @@
+package tests.steam;
+
+import com.codeborne.selenide.Selenide;
+import config.AuthConfig;
+import helpers.Android;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import tests.TestBase;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static io.qameta.allure.Allure.step;
+import static java.time.temporal.ChronoUnit.MILLIS;
+
+public class MabielSteamTests extends TestBase {
+
+    static AuthConfig config = ConfigFactory.create(AuthConfig.class, System.getProperties());
+
+    @Test
+    @Android
+    @Order(1)
+    @DisplayName("Авторизация в приложении")
+    void openApp() {
+        step("Авторизация", () -> {
+            $$(AppiumBy.className("android.widget.EditText")).get(0).click();
+            $$(AppiumBy.className("android.widget.EditText")).get(0).sendKeys(config.steamName());
+            $$(AppiumBy.className("android.widget.EditText")).get(1).click();
+            $$(AppiumBy.className("android.widget.EditText")).get(1).sendKeys(config.steamPassword());
+            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]")).click();
+            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.TextView")).shouldHave(visible).shouldHave(text("WELCOME TO"));
+           // Selenide.sleep(5000);
+            swipeRightQuick(4000);
+            swipeRightQuick(4000);
+            swipeRightQuick(4000);
+        });
+    }
+
+    @Test
+    @Android
+    @Disabled
+    @Order(2)
+    @DisplayName("Кнопка поиска игр")
+    void searchGames() {
+        step("Поиск игры", () -> {
+            $$(AppiumBy.className("android.widget.EditText")).get(0).click();
+            $$(AppiumBy.className("android.widget.EditText")).get(0).sendKeys(config.steamName());
+            $$(AppiumBy.className("android.widget.EditText")).get(1).click();
+            $$(AppiumBy.className("android.widget.EditText")).get(1).sendKeys(config.steamPassword());
+            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]")).click();
+            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.TextView")).shouldHave(visible).shouldHave(text("WELCOME TO"));
+
+
+            swipeUpQuick(100);
+            swipeUpQuick(100);
+
+
+            sleep(3000);
+            $$(AppiumBy.className("android.view.ViewGroup")).get(1).click();
+            sleep(5000);
+            $(AppiumBy.className("android.widget.EditText")).sendKeys("Cuphead" + "\n");
+            sleep(5000);
+            $(AppiumBy.xpath("//android.view.View[@content-desc=\"blank\"]/android.widget.Image")).click();
+            sleep(5000);
+            $$(AppiumBy.className("android.view.View")).get(1).shouldHave(text("Cuphead - The Delicious Last Course"));
+        });
+    }
+
+    @Test
+    @Android
+    @Disabled
+    @DisplayName("Successful search")
+    void elvisPresleyTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Elvis Presley");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    //листать в верх
+    public void swipeUp(int timeOfSwipeMs) {
+        TouchAction action = new TouchAction((PerformsTouchActions) getWebDriver());
+        Dimension size = getWebDriver().manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+
+        action
+                .press(point(x, start_y))
+                .waitAction(WaitOptions.waitOptions(Duration.of(timeOfSwipeMs, MILLIS)))
+                .moveTo(point(x, end_y))
+                .release()
+                .perform();
+    }
+
+    public void swipeUpQuick(int i) {
+        swipeUp(2000);
+    }
+
+    //листать в лево
+    public void swipeRight(int timeOfSwipeMs) {
+        TouchAction action = new TouchAction((PerformsTouchActions) getWebDriver());
+        Dimension size = getWebDriver().manage().window().getSize();
+        int endx = (int) (size.width * 0.20);
+        int startx = (int) (size.width * 0.80);
+        int starty = size.height / 2;
+
+        action
+                .press(point(starty, startx))
+                .waitAction(WaitOptions.waitOptions(Duration.of(timeOfSwipeMs, MILLIS)))
+                .moveTo(point(starty, endx))
+                .release()
+                .perform();
+    }
+
+    public void swipeRightQuick(int i) {
+        swipeRight(2000);
+    }
+}
